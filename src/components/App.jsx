d41b1301b3685b67bdc369e1da19f7bc31939cc4
OleksiiPanaioti <cavedzgn@gmail.com>
2023-04-27 10:24:08 +0300

    localstorage added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,30 @@ import { Layout } from './Layout/Layout';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
 
     filter: '',
   };
+
+  componentDidMount() {
+    const savedPhones = localStorage.getItem('contacts');
+    if (savedPhones !== null) {
+      this.setState({
+        contacts: JSON.parse(savedPhones),
+      });
+    } else {
+      this.setState({
+        contacts: [],
+      });
+    }
+    console.log(savedPhones);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = newContact => {
     const { contacts } = this.state;
