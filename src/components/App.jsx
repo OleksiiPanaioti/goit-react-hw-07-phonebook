1cbc43497ca38c7679b4714cd2325bda797a4a6b
OleksiiPanaioti <cavedzgn@gmail.com>
2023-05-08 10:53:27 +0300

    finished refactorinf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { nanoid } from 'nanoid';
 import { Contacts } from './contactsList/ContactsList';
 import { Filter } from './FilterPack/Filter';
 
 import { PhoneForm } from './FormInput/FormInput';
 import { GlobalStyle } from './GlobalStyles';
 import { Layout } from './Layout/Layout';
+import Massage from './massage/massage';
 
 // class App extends Component {
 //   state = {
@@ -80,7 +82,7 @@ import { Layout } from './Layout/Layout';
 
 const App = () => {
   const [contacts, setContacts] = useState(
-    () = JSON.parse(windown.localstorage.getItem('contacts')) ?? []
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
   );
 
   const [filter, setFilter] = useState('');
@@ -91,23 +93,50 @@ const App = () => {
       firstRender.current = false;
       return;
     }
-    window.localStorage.setItem('contacts', JSON.stringify(contacts))
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  
+  const addContact = ({ name, number }) => {
+    const newContact = { id: nanoid(), name, number };
 
+    contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
+      ? alert(`${name} u ve already entered it,bro`)
+      : setContacts(state => [...state, newContact]);
+  };
 
+  const handleContact = contactId => {
+    setContacts(s => s.filter(contact => contact.id !== contactId));
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const getContactOnFilter = () => {
+    if (filter !== '') {
+      const normalizedFilter = filter.toLowerCase();
+
+      return contacts.filter(({ name }) =>
+        name.toLowerCase().includes(normalizedFilter)
+      );
+    }
+    return contacts;
+  };
+
+  const renderContact = getContactOnFilter();
 
   return (
     <Layout>
       <h2>Phonebook</h2>
-      <PhoneForm onSave={this.addContact} contacts={contacts} />
+      <PhoneForm onSave={addContact} contacts={contacts} />
       <h2>Contacts</h2>
-      <Filter search={filter} onSearch={this.searchContact} />
+      <Filter search={filter} onSearch={changeFilter} />
       {renderContact && (
-        <Contacts contacts={renderContact} onDelete={this.deleteContact} />
+        <Contacts contacts={getContactOnFilter()} onDelete={handleContact} />
       )}
       <GlobalStyle />
     </Layout>
   );
 };
+
+export default App;
---
+++ src/components/massage/massage.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import { Info } from './massage.styled';
+
+const Massage = ({ info }) => {
+  return (
+    <>
+      <Info>{info}</Info>
+    </>
+  );
+};
+
+Massage.propTypes = {
+  info: PropTypes.string.isRequired,
+};
+
+export default Massage;
